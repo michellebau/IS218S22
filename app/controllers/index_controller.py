aab52bb2f8743dc06735a838634e84d3e219ad59
Michelle Bautista <aermichelle@gmail.com>
2022-02-08 10:40:08 -0500

    updated name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Michelle"
+        name = "Michelle Bautista"
         return render_template('index.html', name=name)
